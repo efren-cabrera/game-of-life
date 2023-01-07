ac41af2edc70c6582ae709fe34054cad714cea0d
Efren Cabrera <efren@cabrera.dev>
2023-01-07 09:29:12 -0800

    Add method to get size

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -10,6 +10,7 @@ namespace GameOfLife::Internal {
         const int at(const int &row, const int &column) const;
         const std::vector<std::vector<int>> getData() const;
         const std::vector<int> getNeighbors(const int &row, const int &column) const;
+        const std::pair<int, int> getSize() const { return size; }
     private:
         std::vector<std::vector<int>> m_matrix;
         std::pair<int, int> size;
