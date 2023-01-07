1082933c501df3b7c09678501d07471e847c675c
Efren Cabrera <efren@cabrera.dev>
2023-01-06 21:01:09 -0800

    Add function to get neighbors

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include <vector>
+#include <utility>
 
 namespace GameOfLife::Internal {
     class Matrix
     {
     public:
         Matrix(const std::vector<std::vector<int>> &matrix);
-        int at(const int &row, const int &column);
-       const std::vector<std::vector<int>> getData() const;
+        const int at(const int &row, const int &column) const;
+        const std::vector<std::vector<int>> getData() const;
+        const std::vector<int> getNeighbors(const int &row, const int &column) const;
     private:
         std::vector<std::vector<int>> m_matrix;
+        std::pair<int, int> size;
     };
 }
