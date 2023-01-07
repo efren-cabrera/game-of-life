b55f7614235639bdfb59018f0b603d6ded4439ac
Efren Cabrera <efren@cabrera.dev>
2023-01-06 21:03:07 -0800

    Add update method
    This method will be used to update the state of the game

--- include/Simulator.hpp
+++ include/Simulator.hpp
@@ -6,6 +6,7 @@ namespace GameOfLife {
     public:
         Simulator(Internal::Matrix &intialState) : m_actual{intialState} {};
         Internal::Matrix getCurrentState();
+        void update();
     private:
         Internal::Matrix m_actual;
         const int m_neighborsThreshold{3};
