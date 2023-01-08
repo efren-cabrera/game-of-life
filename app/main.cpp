1b542b61bfa0869496b95dfa3537cf28440ef741
Efren Cabrera <efren@cabrera.dev>
2023-01-07 20:51:49 -0800

    Add program to run game

--- app/main.cpp
+++ app/main.cpp
@@ -2,11 +2,29 @@
 #include <vector>
 
 #include <Matrix.hpp>
+#include <Simulator.hpp>
+#include <OutputHelper.hpp>
+#include <OutputHelperPPM.hpp>
+#include <MatrixInitializer.hpp>
+
+#include <thread>
 
 int main()
 {
-    std::vector<std::vector<int>> testVector{{5, 6, 7}, {7, 4, 3}};
-    Matrix matrix = Matrix(testVector);
-    matrix.printMatrix();
+    GameOfLife::Utils::MatrixInitializer init{};
+    auto rows{600};
+    auto columns{800};
+    
+    GameOfLife::Internal::Matrix initialStateMatrix{init.createMatrix(rows, columns)};
+    GameOfLife::Simulator simulator = GameOfLife::Simulator(initialStateMatrix);
+    GameOfLife::OutputHelper *outPPM = new GameOfLife::OutputHelperPPM("out/test");
+
+    for (int i{0}; i < 50; i++) {
+        *outPPM << simulator.getCurrentState();
+        simulator.update();
+        std::cout << "Frame " << i + 1 << " done." << std::endl;
+        std::cout << "--------------------" << std::endl;
+    }
+
     return 0;
 }
