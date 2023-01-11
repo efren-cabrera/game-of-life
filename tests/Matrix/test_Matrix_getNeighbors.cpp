89d38186595d78b0f2eec40a6a440791b6470e4e
Efren Cabrera <efren@cabrera.dev>
2023-01-10 19:03:11 -0800

    Remove logs in terminal

--- tests/Matrix/test_Matrix_getNeighbors.cpp
+++ tests/Matrix/test_Matrix_getNeighbors.cpp
@@ -2,13 +2,11 @@
 * Check that getNeighbors return expected values
 */
 
-#include <iostream>
 #include <vector>
 #include <Matrix.hpp>
 
 int main()
 {
-    std::cout << "Test starting" << std::endl;
     std::vector<std::vector<int>> testVector{{5, 6, 7}, {7, 4, 3}};
     GameOfLife::Internal::Matrix matrix = GameOfLife::Internal::Matrix(testVector);
     std::vector v1{6, 7, 4};
@@ -16,6 +14,5 @@ int main()
 
     std::vector v2{5, 7, 7, 4, 3};
     assert(matrix.getNeighbors(0, 1) == v2);
-    std::cout << "Test succeeded" << std::endl;
     return 0;
 }
--- tests/Matrix/test_Matrix_getSize.cpp
+++ tests/Matrix/test_Matrix_getSize.cpp
@@ -2,17 +2,14 @@
 * Check that the the getSize method return the expected size
 */
 
-#include <iostream>
 #include <vector>
 #include <Matrix.hpp>
 
 int main()
 {
-    std::cout << "Test starting" << std::endl;
     std::vector<std::vector<int>> testVector{{5, 6, 7}, {7, 4, 3}};
     GameOfLife::Internal::Matrix matrix = GameOfLife::Internal::Matrix(testVector);
     assert(matrix.getSize().first == 2);
     assert(matrix.getSize().second == 3);
-    std::cout << "Test succeeded" << std::endl;
     return 0;
 }
