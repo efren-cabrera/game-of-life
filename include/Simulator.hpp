97dc4af7d203e455745d372b4c8eaa2bbe3bd8ec
Efren Cabrera <efren@cabrera.dev>
2023-01-10 17:23:12 -0800

    Add overpu¡opulation rule

--- include/Simulator.hpp
+++ include/Simulator.hpp
@@ -12,7 +12,8 @@ namespace GameOfLife
 
     private:
         Internal::Matrix m_actual;
-        const int m_neighborsThreshold{3};
+        const int m_neighborsThreshold{2};
+        const int m_neighborsThresholdMax{3};
         void processBlock(std::vector<int> block, std::vector<std::vector<int>> &state);
     };
 }
