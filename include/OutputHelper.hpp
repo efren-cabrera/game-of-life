f0f8486c4e60736ce7bac11970688d12bf1f75cb
Efren Cabrera <efren@cabrera.dev>
2023-01-15 10:10:26 -0800

    Fix style

--- include/OutputHelper.hpp
+++ include/OutputHelper.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <iostream>
 #include <Matrix.hpp>
 
 namespace GameOfLife
--- src/OutputHelperPPM.cpp
+++ src/OutputHelperPPM.cpp
@@ -4,35 +4,35 @@
 
 namespace GameOfLife
 {
-    void OutputHelperPPM::operator<<(const Internal::Matrix &matrix)
-    {
-        auto dimY = matrix.getSize().first;
-        auto dimX = matrix.getSize().second;
-        std::string fileName{m_baseFileName + "_" + std::to_string(currentKeyframe) + ".ppm"};
-        std::ofstream ofs(fileName, std::ios_base::out | std::ios_base::binary);
+	void OutputHelperPPM::operator<<(const Internal::Matrix& matrix)
+	{
+		auto dimY = matrix.getSize().first;
+		auto dimX = matrix.getSize().second;
+		std::string fileName{ m_baseFileName + "_" + std::to_string(currentKeyframe) + ".ppm" };
+		std::ofstream ofs(fileName, std::ios_base::out | std::ios_base::binary);
 
-        ofs << "P6" << std::endl
-            << dimX << ' ' << dimY << std::endl
-            << "255" << std::endl;
+		ofs << "P6" << std::endl
+			<< dimX << ' ' << dimY << std::endl
+			<< "255" << std::endl;
 
-        for (auto row : matrix.getData())
-        {
-            for (auto column : row)
-            {
-                if (column == 1)
-                {
-                    // red, green, blue
-                    ofs << (char)(61) << (char)(166) << (char)(63);
-                }
-                else
-                {
-                    ofs << (char)(0) << (char)(0) << (char)(0);
-                }
-            }
-        }
+		for (auto row : matrix.getData())
+		{
+			for (auto column : row)
+			{
+				if (column == 1)
+				{
+					// red, green, blue
+					ofs << (char)(61) << (char)(166) << (char)(63);
+				}
+				else
+				{
+					ofs << (char)(0) << (char)(0) << (char)(0);
+				}
+			}
+		}
 
-        ofs.close();
-        currentKeyframe++;
-    }
+		ofs.close();
+		currentKeyframe++;
+	}
 
 }
